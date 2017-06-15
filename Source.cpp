31bf052c0be6bb3b8cad99cec988cc28bf013af1
Gabriela Grozeva <gg_grozeva@mail.bg>
2017-06-15 12:29:17 +0300

    new wrong algorithm

--- Source.cpp
+++ Source.cpp
@@ -1,59 +1,95 @@
-#include<conio.h>
 #include<cstdlib>
 #include<ctime>
 #include<iostream>
 #include<math.h>
 #include<vector>
 #include<cstdlib>
-#include<windows.h>
+#include<thread>
 
 const int numThreads = 2;
 
 using namespace std;
 long long d = 0;
-int det(int n, std::vector<std::vector<int>>& mat)
-{
+
+
+void matrixInit (int** &matrix, int size) {
+    matrix = new int*[size];
+	srand((unsigned)time(0));
+    for (int i = 0; i <size; i++ ) {
+        matrix[i] = new int[size];
+        for (int j = 0; j < size; j++) {
+           matrix[i][j] = rand()%10;
+           cout << matrix[i][j]<< " ";
+        }
+        cout << endl;
+    }
+}
+
+void coutMatrix(int** &matrix, int n) {
+    for (int i = 0; i < n; i++ ) {
+        for (int j = 0; j < n; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int** matrixAlloc (int size){
+    int** matrix = new int*[size];
+    for (int i = 0; i < size; i++) {
+        matrix[i] = new int[size];
+    }
+    return matrix;
+}
+
+void matrixDealloc (int size, int** &matrix) {
+    for (int i = 0; i < size; i++) {
+        delete [] matrix[i];
+    }
+    delete [] matrix;
+}
+
+int det(int n, int** &mat) {
 
 	if (n == 2) {
 		return ((mat[0][0] * mat[1][1]) - (mat[1][0] * mat[0][1]));
-	} else {
-		std::vector<std::vector<int>> submat(n, std::vector<int>(n));
-		for (int c = 0; c < n; c++)
+	} else if (n > 2) {
+		int** smallerMatrix = matrixAlloc(n-1); 
+        long long a;
+		for (int i = 0; i < n; i++)
 		{
-			int subi = 0; //submatrix's i value
-			for (int i = 1; i < n; i++)
-			{
-				int subj = 0;
-				for (int j = 0; j < n; j++)
-				{
-					if (j == c)
-						continue;
-					submat[subi][subj] = mat[i][j];
-					subj++;
-				}
-				subi++;
-
-			}
-			d = d + (pow(-1, c) * mat[0][c] * det(n - 1, submat));
+            for (int row = 0; row < n; row++) {
+                if ( i == row) {
+                    continue;
+                }
+                for (int col = 1; col < n; col++) {
+                    if(i < row ) {
+                        smallerMatrix[row - 1][col - 1] = mat[row][col];
+                    } else {
+                        smallerMatrix[row][col - 1] = mat[row][col];
+                    }
+                }
+            }
+            cout << "new matrix" << endl;
+            a = det(n - 1, smallerMatrix);
+			d = d + (pow(-1, i) * mat[i][0] * a);
 		}
+        coutMatrix(smallerMatrix, n-1);
+        cout <<"matrix value" << a << endl; 
+        matrixDealloc(n - 1, smallerMatrix);
 	}
 	return d;
 }
 
 int generateAndEvaluateDeterminant(int n) {
-	std::vector<std::vector<int>> A(n, std::vector<int>(n));
-	srand((unsigned)time(0));
-	for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-			A[i][j] = rand()%100;
-			cout << A[i][j] << " ";
-		}
-		cout << endl;
-	}
+	int** A;
+    matrixInit(A, n);
 	int d = det(n, A);
+    matrixDealloc(n, A);
 	return d;
 
 }
+
 int main(int argc, char **argv)
 {
 	int n;
