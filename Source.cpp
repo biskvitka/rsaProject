10228fcef029da5570aaf43ccfc6260f088e6451
Gagriela Grozeva <6okolaaad@mail.bg>
2017-06-18 23:13:42 +0300

    almost there

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,12 @@
 #include<cstdlib>
-#include<ctime>
 #include<iostream>
 #include<math.h>
 #include<vector>
 #include<cstdlib>
 #include<thread>
 #include<chrono>
+#include<thread>
+#include<future>
 
 const int numThreads = 2;
 
@@ -49,14 +50,49 @@ void matrixDealloc (int size, int** &matrix) {
     delete [] matrix;
 }
 
-int det(int n, int** &mat) {
 
+int evaluateSmallerMatrix(int n, int** &mat) {
     long long d = 0;
 	if (n == 2) {
 		return ((mat[0][0] * mat[1][1]) - (mat[1][0] * mat[0][1]));
 	} else if (n > 2) {
-		for (int i = 0; i < n; i++)
-		{
+	    for (int i = 0; i < n; i++) {
+            if ( mat[i][0] == 0) {
+                continue;
+            }
+		    int** smallerMatrix = matrixAlloc(n-1); 
+            for (int row = 0; row < n; row++) {
+                if ( i == row) {
+                    continue;
+                }
+
+                for (int col = 1; col < n; col++) {
+                    if (i < row ) {
+                        smallerMatrix[row - 1][col - 1] = mat[row][col];
+                    } else {
+                        smallerMatrix[row][col - 1] = mat[row][col];
+                    }
+                }
+            }
+	        d = d + (pow(-1, i) * mat[i][0] * 
+                evaluateSmallerMatrix(n - 1, smallerMatrix));
+            matrixDealloc(n - 1, smallerMatrix);
+	    }
+    }
+        return d;
+}
+
+int det(int n, int** &mat, int start, int end) {
+
+            cout << "hi" << endl;
+    long long d = 0;
+	if (n == 2) {
+	    return ((mat[0][0] * mat[1][1]) - (mat[1][0] * mat[0][1]));
+	} else if (n > 2) {
+	    for (int i = start; i < end; i++) {
+            if (mat[i][0] == 0) {
+                continue;
+            }
 		    int** smallerMatrix = matrixAlloc(n-1); 
             for (int row = 0; row < n; row++) {
                 if ( i == row) {
@@ -70,34 +106,61 @@ int det(int n, int** &mat) {
                     }
                 }
             }
-			d = d + (pow(-1, i) * mat[i][0] * det(n - 1, smallerMatrix));
+    	    d = d + (pow(-1, i) * mat[i][0] * evaluateSmallerMatrix(n - 1, smallerMatrix));
             matrixDealloc(n - 1, smallerMatrix);
 		}
 	}
 	return d;
 }
 
-int generateAndEvaluateDeterminant(int n) {
-	int** A;
-    matrixInit(A, n);
-	int d = det(n, A);
-    matrixDealloc(n, A);
-	return d;
 
+
+int generateAndEvaluateDeterminant(int n) {
+    // Create determinant
+    int** A;
+    matrixInit(A, n);
+    
+    // Start threads execution
+    int threadCount = 14;
+    int difference = n / threadCount; 
+    vector<future<int>> results;
+    int start = 0;
+    for (int i = 0;  i < threadCount; ++i, start += difference) {
+        if ( i == (threadCount - 1)) {
+            results.push_back(async(launch::async, det, n, ref(A), start, n));
+        } else {
+            results.push_back(async(launch::async, det, n, ref(A), start, start + difference));
+        }
+    }
+
+    // Get threads results
+    long long result = 0;
+    for (int i = 0; i < threadCount; ++i) {
+            cout << "HERE" << endl;
+        result += results[i].get();
+        
+    }
+
+    // Destroy determinant
+    matrixDealloc(n, A);
+
+    // Return result
+    return result;
 }
 
 int main(int argc, char **argv)
 {
-    auto start = std::chrono::high_resolution_clock::now();	
 
     int n;
     cin >> n;
+    auto start = std::chrono::high_resolution_clock::now();	
+    
     cout << generateAndEvaluateDeterminant(n) << endl;
 
     auto finish = std::chrono::high_resolution_clock::now();	
     cout << "TIMEEEEE: " 
-	<< std::chrono::duration_cast<std::chrono::milliseconds>(finish - start)
-	.count()
+	    << std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start)
+	    .count()
         << " milliseconds" << std::endl;
     return 0;
 }
